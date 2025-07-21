7af77c432df3cc70fab0358fbf510c5846ac712f
Victor L. Ribeiro <victor.lnc.ribeiro@gmail.com.br>
2025-07-21 13:06:50 -0300

    Ajuste no dockerfile do firewall

--- server/firewall/start.sh
+++ server/firewall/start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+source /venv/bin/activate
+
 # Start the firewall manager in the background
 echo "Starting Firewall Manager..."
-python3 /app/firewall_manager.py &
+/venv/bin/python /app/firewall_manager.py
 
 # Wait a moment for the firewall manager to start
 sleep 5
